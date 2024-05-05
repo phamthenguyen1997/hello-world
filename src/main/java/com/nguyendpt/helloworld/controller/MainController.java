b91f7c3973ac5dbc06c65abb2e36ca77da0249e5
Phạm Thế Nguyên <phamthenguyen1997@gmail.com>
2024-05-05 16:28:57 +0700

    E:\Dev\DevTools\apache-maven-3.9.6 update

--- src/main/java/com/nguyendpt/helloworld/controller/MainController.java
+++ src/main/java/com/nguyendpt/helloworld/controller/MainController.java
@@ -1,9 +1,11 @@
 package com.nguyendpt.helloworld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/main")
+@RestController
+@RequestMapping("main")
 public class MainController {
 
 	@GetMapping(value = "/hello")
